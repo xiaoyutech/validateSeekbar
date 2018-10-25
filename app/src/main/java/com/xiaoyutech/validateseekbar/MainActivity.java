1386b3f88fd28fe07ad30d4e5609577f745bc8d5
caizhibiao <caizhibiao@cmhi.chinamobile.com>
2018-10-25 10:02:58 +0800

    OCT REM add resources, 完成UI优化

--- app/src/main/java/com/xiaoyutech/validateseekbar/MainActivity.java
+++ app/src/main/java/com/xiaoyutech/validateseekbar/MainActivity.java
@@ -1,16 +1,20 @@
-package com.validateseekbar;
+package com.xiaoyutech.validateseekbar;
 
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
+
+import com.xiaoyutech.validateseekbar.R;
 
 public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
 
     private TextView tv;
     private SeekBar seekBar;
+    private ImageView ivSeekDone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,10 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         tv = (TextView) findViewById(R.id.tv);
         seekBar = (SeekBar) findViewById(R.id.sb);
         seekBar.setOnSeekBarChangeListener(this);
+        ivSeekDone = (ImageView) findViewById(R.id.iv_seek_done);
+        //这里是个大坑，SeekBar在不同安卓版本OR机型上有着不同表现，默认padding不同
+        //在代码中手动设置padding，避免发生无法"matchparent"的情况
+        seekBar.setPadding(0, 0, 0, 0);
     }
 
     /**
@@ -35,6 +43,9 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
             tv.setVisibility(View.VISIBLE);
             tv.setTextColor(Color.WHITE);
             tv.setText("完成验证");
+            ivSeekDone.setVisibility(View.VISIBLE);
+            seekBar.setThumb(null);
+//            seekBar.setEnabled(false);
         } else {
             tv.setVisibility(View.INVISIBLE);
         }
